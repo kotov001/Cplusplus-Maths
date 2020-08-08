2ae4204fefbeb5ee38a6eddf1c1d66c1015e13dd
kotov001 <rodwellmakoto@msn.com>
2020-08-08 13:15:09 +0200

    Update FibonacciSeries.cpp

--- FibonacciSeries.cpp
+++ FibonacciSeries.cpp
@@ -6,10 +6,10 @@ int fibonacci(int n);
  int fibonacci(int n){
 	if(n == 0)
 	   return n;
-	if(n==1 || n == 0)
+	if(n==1 || n == 0)           //base case
 	   return 1;
 	else
-	    return fibonacci(n -2) + fibonacci(n-3);
+	    return fibonacci(n -1) + fibonacci(n-2);   //recursion
 }
 
 int main(){
